f537c9fdf9309d9b920c27ec4d8fd227859a2726
Yutin Liu (劉宇庭) <Yutin.Liu@mediatek.com>
2015-08-09 23:18:05 +0800

    remove uuid

--- www/ParseInstallation.js
+++ www/ParseInstallation.js
@@ -1,81 +1,6 @@
 var exec = require('cordova/exec');
 
 module.exports = (function() {
-  var _global = this;
-
-  // Unique ID creation requires a high quality random # generator.  We feature
-  // detect to determine the best RNG source, normalizing to a function that
-  // returns 128-bits of randomness, since that's what's usually required
-  var _rng;
-
-  // Allow for MSIE11 msCrypto
-  var _crypto = _global.crypto || _global.msCrypto;
-
-  // Node.js crypto-based RNG - http://nodejs.org/docs/v0.6.2/api/crypto.html
-  //
-  // Moderately fast, high quality
-  if (typeof(_global.require) == 'function') {
-    try {
-      var _rb = _global.require('crypto').randomBytes;
-      _rng = _rb && function() {return _rb(16);};
-    } catch(e) {}
-  }
-
-  if (!_rng && _crypto && _crypto.getRandomValues) {
-    // WHATWG crypto-based RNG - http://wiki.whatwg.org/wiki/Crypto
-    //
-    // Moderately fast, high quality
-    var _rnds8 = new Uint8Array(16);
-    _rng = function whatwgRNG() {
-      _crypto.getRandomValues(_rnds8);
-      return _rnds8;
-    };
-  }
-
-  if (!_rng) {
-    // Math.random()-based (RNG)
-    //
-    // If all else fails, use Math.random().  It's fast, but is of unspecified
-    // quality.
-    var  _rnds = new Array(16);
-    _rng = function() {
-      for (var i = 0, r; i < 16; i++) {
-        if ((i & 0x03) === 0) r = Math.random() * 0x100000000;
-        _rnds[i] = r >>> ((i & 0x03) << 3) & 0xff;
-      }
-
-      return _rnds;
-    };
-  }
-
-  // Maps for number <-> hex string conversion
-  var _byteToHex = [];
-  var _hexToByte = {};
-  for (var i = 0; i < 256; i++) {
-    _byteToHex[i] = (i + 0x100).toString(16).substr(1);
-    _hexToByte[_byteToHex[i]] = i;
-  }
-
-  // uuid v4
-  function uuid() {
-    var rnds = _rng();
-    var bth = _byteToHex;
-    var i = 0;
-
-    // Per 4.4, set bits for version and `clock_seq_hi_and_reserved`
-    rnds[6] = (rnds[6] & 0x0f) | 0x40;
-    rnds[8] = (rnds[8] & 0x3f) | 0x80;
-
-    return  bth[rnds[i++]] + bth[rnds[i++]] +
-            bth[rnds[i++]] + bth[rnds[i++]] + '-' +
-            bth[rnds[i++]] + bth[rnds[i++]] + '-' +
-            bth[rnds[i++]] + bth[rnds[i++]] + '-' +
-            bth[rnds[i++]] + bth[rnds[i++]] + '-' +
-            bth[rnds[i++]] + bth[rnds[i++]] +
-            bth[rnds[i++]] + bth[rnds[i++]] +
-            bth[rnds[i++]] + bth[rnds[i++]];
-  }
-
   var deferred;
   var toReturn;
   if (window.jQuery) {
@@ -122,43 +47,23 @@ module.exports = (function() {
   }
 
   function createInstallation() {
-    var q = deferred();
-
-    var Installation = Parse.Object.extend("_Installation");
-    var installation = new Installation();
-    var platform = device.platform.toLowerCase();
-    var installationId = window.localStorage.getItem('parseInstallationId');
-    if (!installationId) {
-      installationId = uuid();
-    }
-    installation.set('deviceType', platform);
-    installation.set('installationId', installationId);
-    installation.save().then(q.resolve, q.reject);
-    window.localStorage.setItem('parseInstallationId', installationId);
-
-    console.log('create: ' + installationId);
-    return toReturn(q);
+    // var installationId = window.localStorage.getItem('parseInstallationId');
+    // installation.set('installationId', installationId);
+    // window.localStorage.setItem('parseInstallationId', installationId);
   }
 
   function getCurrentInstallation() {
     var q = deferred();
 
     var Installation = Parse.Object.extend("_Installation");
-    var installationObjectId = window.localStorage.getItem('parseInstallationObjectId');
+    var installation = new Installation();
 
-    if (!installationObjectId) {
-      createInstallation().then(q.resolve, q.reject);
-    } else {
-      console.log('get: ' + installationObjectId);
-      var query = new Parse.Query(Installation);
-      query.get(installationObjectId).then(q.resolve, function(e) {
-        console.log('get installation error: ' + JSON.stringify(e));
-        createInstallation().then(q.resolve, q.reject);
-      });
-    }
+    var platform = device.platform.toLowerCase();
+    installation.set('deviceType', platform);
+    installation.save().then(q.resolve, q.reject);
 
     return toReturn(q).then(function(installation) {
-      window.localStorage.setItem('parseInstallationObjectId', installation.id);
+      console.log('save installation: ' + installation.id + ', ' + installation.get('installationId'));
       return installation;
     });
   }
